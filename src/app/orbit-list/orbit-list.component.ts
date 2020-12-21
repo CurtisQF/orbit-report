54182ce91b321af2ce1fcef4937f3bb0e8f85c87
Curtis Moeller <curtisqfarrell@gmail.com>
2020-12-21 17:08:23 -0600

    Step 7

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -8,8 +8,6 @@ import { Satellite } from '../satellite';
 })
 export class OrbitListComponent implements OnInit {
 
-  // typeWarning: boolean = shouldShowWarning();
-
   @Input() satellites: Satellite[];
 
   constructor() { }
@@ -17,4 +15,15 @@ export class OrbitListComponent implements OnInit {
   ngOnInit() {
   }
 
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+  }
 }
